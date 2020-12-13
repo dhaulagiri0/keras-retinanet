bca1ef693d400f5275318e05a646bc02d76ba094
Dhaulagiri0 <yiqinw4@gmail.com>
2020-12-13 14:54:07 +0800

    modified augmentations

--- keras_retinanet/bin/train.py
+++ keras_retinanet/bin/train.py
@@ -240,14 +240,8 @@ def create_generators(args, preprocess_image):
         transform_generator = random_transform_generator(
             min_rotation=-0.1,
             max_rotation=0.1,
-            min_translation=(-0.1, -0.1),
-            max_translation=(0.1, 0.1),
             min_shear=-0.1,
             max_shear=0.1,
-            min_scaling=(0.9, 0.9),
-            max_scaling=(1.1, 1.1),
-            flip_x_chance=0.5,
-            flip_y_chance=0.5,
         )
         visual_effect_generator = random_visual_effect_generator(
             contrast_range=(0.9, 1.1),
